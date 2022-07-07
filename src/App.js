41a57b72b56ca2acef4d3bfa1be103411cefd5a6
Emiliano Scanga <scangaemiliano@gmail.com>
2022-07-07 14:20:53 +0200

    End: esercizio components-03

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { React } from 'react';
+import { Hello } from './Hello';
+
+export class App extends React.components{
+    render(){
+        return(
+            <div>
+                <Hello />
+            </div>
+        );
+    };
+};
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,7 @@
 import { React } from 'react';
 
-class Hello extends React.components{
+export class Hello extends React.components{
     render(){
         return <h1>Hello World!</h1>
-    }
+    };
 };
