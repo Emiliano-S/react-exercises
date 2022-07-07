362c6ef5bb30c29949e447479278190093093da8
Emiliano Scanga <scangaemiliano@gmail.com>
2022-07-07 15:02:03 +0200

    End: esercizio components-07

--- src/Hello.js
+++ src/Hello.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Message } from './Message';
 
 export class Hello extends React.Component{
     render(){
         return (
             <div>
                 <h1>Hello World!</h1>
-                <p>What a beatiful day!</p>
+                <Message />
             </div>
         );
     };
