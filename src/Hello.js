e81d29dd79cc20513d9350e200eadf4c30042ac4
Emiliano Scanga <scangaemiliano@gmail.com>
2022-07-07 14:36:48 +0200

    Fix: errore nell'import

--- src/Hello.js
+++ src/Hello.js
@@ -1,6 +1,6 @@
-import { React } from 'react';
+import React from 'react';
 
-class Hello extends React.components{
+class Hello extends React.Component{
     render(){
         return <h1>Hello World!</h1>
     }
