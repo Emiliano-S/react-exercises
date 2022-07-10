62be68a720c277e68e7dee2df1cfcda5b2de05f8
Emiliano G. Scanga <scangaemiliano@gmail.com>
2022-07-10 10:15:24 +0200

    End: esercizio props-02

--- src/index.js
+++ src/index.js
@@ -7,3 +7,9 @@ const root = createRoot(container);
 root.render(<App />);
 
 
+/*
+What happens if no name prop is passed to the Welcome component? Can you set a default value for the name prop?
+Se non passiamo name la stessa verrà valorizzato come undefined.
+Per settare un valore di default possiamo utilizzare un'operatore logico "||" per settare un valore di fallback,
+in questo modo se la prop non viene dichiarata si mostrerà il valore di fallback.
+*/
