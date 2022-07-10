34ba01c6cc05412988b72fe888ad4d7e01668c6d
Emiliano G. Scanga <scangaemiliano@gmail.com>
2022-07-10 10:25:03 +0200

    End: esercizio props-04

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component{
         return(
             <div>
                 <Hello />
-                <Welcome name="Carlo" age={27}/>
+                <Welcome name={<strong>Carlo</strong>} age={27}/>
             </div>
         );
     };
--- src/index.js
+++ src/index.js
@@ -8,8 +8,16 @@ root.render(<App />);
 
 
 /*
+Props-02
 What happens if no name prop is passed to the Welcome component? Can you set a default value for the name prop?
 Se non passiamo name la stessa verrà valorizzato come undefined.
 Per settare un valore di default possiamo utilizzare un'operatore logico "||" per settare un valore di fallback,
 in questo modo se la prop non viene dichiarata si mostrerà il valore di fallback.
+*/
+
+/*
+Props-04
+What happens if the name prop is a JSX expression instead of a string? How do you pass a prop that contains a JSX expression?
+Per assegnare qualsiasi valore, che non sia una stringa, a un prop, bisogna utilizzare le { }, quindi in questo caso, per passare name
+con strong in JSX dobbiamo assegnarlo in questo modo name={<strong>Carlo</strong>}.
 */
