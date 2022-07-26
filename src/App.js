909537ce889fbc0cb204fbaa81d8d43c136fdfab
Emiliano G. Scanga <scangaemiliano@gmail.com>
2022-07-26 17:10:59 +0200

    End: esercizio forms-05

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Login } from './Login';
+import { UncontrolledLogin } from './UncontrolledLogin';
 
 
 export class App extends React.Component{
@@ -9,7 +9,7 @@ export class App extends React.Component{
     render(){
         return(
             <div>
-                <Login data={this.onLoginData}/>
+                <UncontrolledLogin />
             </div>
         );
     };
