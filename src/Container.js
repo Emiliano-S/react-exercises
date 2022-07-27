2a90c4f1c3ff0d75edd0c226c24b6bdc8dfc9300
Emiliano Scanga <scangaemiliano@gmail.com>
2022-07-27 12:32:41 +0200

    End: esercizio component-composition-02

--- src/Container.js
+++ src/Container.js
@@ -4,6 +4,8 @@ export class Container extends React.Component{
     render(){
         return(
             <div className="container">
+                <h1>{this.props.title}</h1>
+                <hr />
                 {this.props.children}
             </div>
         )
--- src/Welcome.js
+++ src/Welcome.js
@@ -6,7 +6,7 @@ import { Container } from './Container';
 export class Welcome extends React.Component{
     render(){
         return(
-            <Container>
+            <Container title="This is a title!">
                 <p>Hello, {this.props.name}!</p>
                 <Age age={this.props.age}/>
             </Container>
